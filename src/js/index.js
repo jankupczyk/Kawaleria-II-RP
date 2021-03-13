b9c8118a1a5ea2a3f9897e1f00056587aed4406f
jankupczyk <73750630+jankupczyk@users.noreply.github.com>
2021-03-13 20:57:01 +0100

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+totop = document.getElementById("BTN-to-top")
+window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        totop.style.display = "block";
+    } else {
+        totop.style.display = "none";
+    }
+    var winscroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winscroll / height) * 100;
+    document.getElementById('progress-bar').style.width = `${scrolled}%`;
+    document.getElementById('progress-bar').title = `Zwiedzono ${(scrolled).toFixed(1)}% strony`;
+    document.getElementById('progress-bar-container').title = `Zostało ${(100 - scrolled).toFixed(1)}% strony do zwiedzenia`
+}
+
+const togglebtn = document.getElementsByClassName('toggle-btn')[0]
+const links = document.getElementsByClassName('nav-links')[0]
+
+togglebtn.addEventListener('click', () => {
+    links.classList.toggle('active')
+})
+
+
+function TOTOP() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
